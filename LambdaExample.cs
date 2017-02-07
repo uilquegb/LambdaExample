4c98ed10d76d0a54427ecb2d2aad1ee33a17dcd5
Uilque Messias Oliveira da Cruz <Uilque Messias Oliveira da Cruz>
2017-02-07 02:53:29 -0200

    Add Aws Lambda Function with C#

---
+++ LambdaExample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+
+namespace Example
+{
+    public class LambdaExample
+    {
+
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public Person FunctionHandler(ILambdaContext context)
+        {
+            Console.WriteLine("Hello World!");
+
+            context.Logger.LogLine("Hello world");
+            return new Person("Uilque Messias", 25);
+        }
+    }
+
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        [JsonProperty("name")]
+        public String Name { get; set; }
+
+        [JsonProperty("age")]
+        public int Age { get; set; }
+
+    }
+
+}
